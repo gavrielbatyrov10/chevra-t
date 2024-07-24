01183e5c227da271e340ef419a428d8425e0c037
gavriel <gbatyrov10@gmail.com>
2024-07-24 18:16:29 -0400

    updated ui

--- chevra/src/Landing.jsx
+++ chevra/src/Landing.jsx
@@ -13,14 +13,12 @@ export default function Landing() {
         </figure>
         <div className="wrapper">
           <p className="landing__header">ONE CALL FIX ALL!</p>
-          <p className="landing__para">
-            At Handy Chevra, we offer expert handyman services for all your
-            household needs. From electrical and plumbing repairs to drywall
-            patchups and appliance installations, our skilled team has you
-            covered. We also handle rubbish removal, light hauling, mice
-            proofing, window shade repairs, furniture assembly, and
-            post-construction touch-ups. No job is too big or small—trust us to
-            get it done with professionalism and care.
+          <p className="landing__para">Handy Chevra</p>
+          <p className="landing__para--mobile">
+            Handy Chevra offers expert handyman services for all your household
+            needs. From electrical and plumbing repairs to appliance
+            installations and rubbish removal, our skilled team has you covered.
+            Trust us for professional and careful service.
           </p>
         </div>
       </div>
