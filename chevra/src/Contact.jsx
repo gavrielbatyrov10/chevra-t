1c499323582e7f62c9ed6865548d6480b206f907
gavriel <gbatyrov10@gmail.com>
2024-07-24 02:17:12 -0400

    updated menu

--- chevra/src/Contact.jsx
+++ chevra/src/Contact.jsx
@@ -11,16 +11,12 @@ function Contact() {
 
   return (
     <div className="Contact" id="contact">
-      <h2 data-aos="fade-in">Contact Us</h2>
+      <h2 className="contact__header" data-aos="fade-in">
+        Contact Us
+      </h2>
       <p>Email: info@handymanservices.com</p>
       <p>Phone: (123) 456-7890</p>
       <p>Address: 123 Handyman St, Service City, ST 12345</p>
-      <div data-aos="fade-down">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur ut
-        dolore tempore quis accusantium soluta dolorum laboriosam laudantium in
-        distinctio atque, sequi necessitatibus, saepe magnam! Perferendis iste
-        illo eum cum.
-      </div>
     </div>
   );
 }
